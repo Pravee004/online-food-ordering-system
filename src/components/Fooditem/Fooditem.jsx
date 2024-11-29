7ca1e171352b7ce7b21fb386f06678f2ee6f6e6d
Pravee004 <140936722+Pravee004@users.noreply.github.com>
2024-11-29 22:04:34 +0530

    Add files via upload

---
+++ src/components/Fooditem/Fooditem.jsx
@@ -0,0 +1,35 @@
+import React, { useContext} from 'react';
+import './Fooditem.css';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
+
+const Fooditem = ({ id, name, price, description, image }) => {  
+  
+  const {cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+
+  return (
+    <div className='food-item'>
+      <div className='food-item-img-container'>
+        <img className='food-item-image' src={image} alt={name} />
+        {!cartItems[id]
+          ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="Add" />
+          : <div className='food-item-counter'>
+              <button onClick={() => removeFromCart(id)}>-</button>
+              <span>{cartItems[id]}</span>
+              <button onClick={() => addToCart(id)}>+</button>
+            </div>
+        }
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="Rating" />
+        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Fooditem;
